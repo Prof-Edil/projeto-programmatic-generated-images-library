ed20655bda0a723d6da38ec6fd61f1247ff40f6c
GiordanoSM <giordano@suffert.com>
2021-05-22 21:57:34 -0300

    Pintando todos os blocos da imagem

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,6 @@
 import Lib
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
---fish, triangle or testFig
 
 
 testOut1 = [CubicBezier (V2 1.00 1.00) (V2 0.85 0.95) (V2 0.70 0.95) (V2 0.50 0.98)
@@ -9,7 +8,9 @@ testOut1 = [CubicBezier (V2 1.00 1.00) (V2 0.85 0.95) (V2 0.70 0.95) (V2 0.50 0.
            ]
 testOut2 = over (rot45 (Lib.flip (rot testOut1))) (rot (rot (rot45 (Lib.flip (rot testOut1))))) 
 testOut3 = transform (multFst (-1)) ((rot.rot.rot) (Lib.flip testOut1))
-testFig = over (over testOut1 testOut2) testOut3
+testFig1 = over (over testOut1 testOut2) testOut3
+
+testFig = triangle --fish, triangle or testFig1
 
 test_img = drawAndWrite "img_tests/img.png" testFig
 test_rot = drawAndWrite "img_tests/rot.png" (rot testFig)
@@ -21,11 +22,12 @@ test_t = drawAndWrite "img_tests/t.png" (t testFig)
 test_quartet = drawAndWrite "img_tests/quartet.png" (quartet testFig testFig testFig testFig)
 test_side2 = drawAndWrite "img_tests/side2.png" (side 2 testFig)
 test_corner2 = drawAndWrite "img_tests/corner2.png" (corner 2 testFig)
+test_squarelimit1 = drawAndWrite "img_tests/squarelimit1.png" (squarelimit 1 testFig)
 test_squarelimit2 = drawAndWrite "img_tests/squarelimit2.png" (squarelimit 2 testFig)
 test_squarelimit3 = drawAndWrite "img_tests/squarelimit3.png" (squarelimit 3 testFig)
 test_squarelimit4 = drawAndWrite "img_tests/squarelimit4.png" (squarelimit 4 testFig)
 
-test_simple_img = drawAndWrite2 "img_tests/simplefig.png" (arcLimit 1 arc) 1
+test_simple_img = drawAndWrite2 "img_tests/simplefig.png" (arcLimit 0 arc) 0
 
 main :: IO ()
 main = do
@@ -39,6 +41,7 @@ main = do
   test_quartet
   test_side2
   test_corner2
+  test_squarelimit1
   test_squarelimit2
   test_squarelimit3
   test_squarelimit4
